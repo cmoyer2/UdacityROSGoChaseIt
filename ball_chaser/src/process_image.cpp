9461e7873e22ab38682b96992c74b038eaee16b3
Cameron Moyer <cmoyer2@hotmail.com>
2019-03-07 14:05:33 -0800

    Changes to find ball in image.

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -25,12 +25,19 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
-
+    bool foundWhilteBall = false;
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
-    ROS_INFO("Image - height = %d, width = %d",img.height, img.width);
+    for (int i = 0; i < img.height * img.step; i++) {
+        if (img.data[i] == white_pixel) {
+            foundWhilteBall = true;
+            ROS_INFO("Found white ball at " + i);
+            break;
+        }
+    }
+
 }
 
 int main(int argc, char** argv)
